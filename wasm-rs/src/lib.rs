e78f4f83256d54868c614af061129554c027884c
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2024-03-14 13:19:18 +0800

    :sparkls: Data structs

--- wasm-rs/src/lib.rs
+++ wasm-rs/src/lib.rs
@@ -1,3 +1,5 @@
+use serde::Deserialize;
+use toml;
 use wasm_bindgen::prelude::*;
 
 // import Javascript's alert method to Rust
@@ -10,4 +12,73 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet(str: &str) {
     alert(&format!("Bye, {}!", str));
+}
+
+#[allow(dead_code)]
+#[wasm_bindgen]
+#[derive(Deserialize)]
+pub struct Bunyian {
+    rantau: RantauBunyian,
+    kaedah: KaedahBunyian,
+    jawi: String,
+    jenis_jawi: JenisJawi,
+    rumi: String,
+    ipa: String,
+}
+
+#[wasm_bindgen]
+impl Bunyian {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self {
+            rantau: RantauBunyian::Dwibibir,
+            kaedah: KaedahBunyian::Sengauan,
+            jawi: "".into(),
+            jenis_jawi: JenisJawi::Kongsi,
+            rumi: "".into(),
+            ipa: "".into(),
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn toml_to_bunyians(data: String) -> Vec<Bunyian> {
+    if let Ok(v) = toml::from_str(&data) {
+        v
+    } else {
+        alert(&format!("Error reading string."));
+        vec![]
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Deserialize, Clone, Copy)]
+pub enum RantauBunyian {
+    Dwibibir,
+    BibirGusi,
+    LelangitGusi,
+    Lelangit,
+    LelangitLembut,
+    AnakTekak,
+    Tekak,
+}
+
+#[wasm_bindgen]
+#[derive(Deserialize, Clone, Copy)]
+pub enum KaedahBunyian {
+    Sengauan,
+    LetusanBersuara,
+    LetusanTakBersuara,
+    GeseranBersuara,
+    GeseranTakBersuara,
+    MalaranTakGeser,
+    Getaran,
+}
+
+#[wasm_bindgen]
+#[derive(Deserialize, Clone, Copy)]
+pub enum JenisJawi {
+    Kongsi,
+    Ciptaan,
+    Arab,
 }
